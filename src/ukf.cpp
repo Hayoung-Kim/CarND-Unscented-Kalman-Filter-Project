f658380a998fcca9a81bc1f5ef62a024baa3de53
Hayoung-Kim <altairyoung@gmail.com>
2017-05-29 20:48:14 +0900

    implement: (prediction step) predict mean and covariance

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -52,8 +52,29 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  ///* State dimension
+  int n_x_ = 5;
+
+  ///* Augmented state dimension
+  int n_aug_ = 7;
+
+  ///* Sigma point spreading parameter
+  double lambda_ = 3-n_x_;
+
+  ///* Weights of sigma points
+  VectorXd weights_ = VectorXd(2*n_aug_+1);;
+
+  ///* the current NIS for radar
+  double NIS_radar_ = 0.0;
+
+  ///* the current NIS for laser
+  double NIS_laser_ = 0.0;
+
+  ///* initially set to false, set to true in first call of ProcessMeasurement
   is_initialized_ = false;
 
+  ///* initial time
   previous_timestamp_ = 0;
 }
 
@@ -207,8 +228,42 @@ void UKF::Prediction(double delta_t) {
   }
 
   
-  // predict mean and covariance
+  //--------------- predict mean and covariance ------------------//
   
+  //create vector for predicted state
+  VectorXd x = VectorXd(n_x_);
+
+  //create covariance matrix for prediction
+  MatrixXd P = MatrixXd(n_x_, n_x_);
+
+  // set weights
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0;
+  for (int i=1; i<2*n_aug_+1; i++) {  //2n+1 weights
+    double weight = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight;
+  }
+
+  //predicted state mean
+  x.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+    x = x+ weights_(i) * Xsig_pred.col(i);
+  }
+
+  //predicted state covariance matrix
+  P.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+
+    // state difference
+    VectorXd x_diff = Xsig_pred.col(i) - x;
+
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    P = P + weights_(i) * x_diff * x_diff.transpose() ;
+  }
+
 }
 
 /**
